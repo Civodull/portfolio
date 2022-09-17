f5a5647fa936a58a9a48def9567c76d549504174
Civodull <olivierndi57@gmail.com>
2022-09-17 17:54:46 +0000

    First portfolio commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Typing effect animation */
+
+var typed = new Typed(".typing", {
+    strings:["","Web developper","Concepteur et programmeur IoT","Data Analyste","Data Scientist","Data Architect"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
